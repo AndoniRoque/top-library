27eb4fac793bc007c00f63b002ef62eae9adaf43
AndoniRoque <roque.andoni@gmail.com>
2024-02-16 08:52:37 -0300

    player constructor

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,15 @@
-alert("hola")
+function Player(name, marker) {
+    this.name = name;
+    this.marker = marker;
+    this.welcomeMessage = () => {
+        console.log("hello ", this.name, " your maker is ", this.marker);
+    }
+}
+
+const player = new Player("steve", "x");
+console.log(player.name);
+console.log(player.welcomeMessage())
+
+const player2 = new Player("alex", "0");
+
+console.log(player2.welcomeMessage());
