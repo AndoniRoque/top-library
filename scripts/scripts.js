1338b023fa478397740b11e11e94e387b57c818f
AndoniRoque <roque.andoni@gmail.com>
2024-02-16 09:13:13 -0300

    book constructor exercise

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,28 @@
-function Player(name, marker) {
-    this.name = name;
-    this.marker = marker;
-    this.welcomeMessage = () => {
-        console.log("hello ", this.name, " your maker is ", this.marker);
+// function Player(name, marker) {
+//     this.name = name;
+//     this.marker = marker;
+//     this.welcomeMessage = () => {
+//         console.log("hello ", this.name, " your maker is ", this.marker);
+//     }
+// }
+
+// const player = new Player("steve", "x");
+// console.log(player.name);
+// console.log(player.welcomeMessage())
+
+// const player2 = new Player("alex", "0");
+
+// console.log(player2.welcomeMessage());
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () => {
+        return this.title + " by " + this.author + ", " + this.pages + ", " + this.read
     }
 }
 
-const player = new Player("steve", "x");
-console.log(player.name);
-console.log(player.welcomeMessage())
-
-const player2 = new Player("alex", "0");
-
-console.log(player2.welcomeMessage());
+const book = new Book("House of Leaves", "Mark z. Danielewski", "500", "read");
+console.log(book.info());
