0fdaf48dbde8ec1cc13e87c3a98827516f3e9989
AndoniRoque <roque.andoni@gmail.com>
2024-02-27 08:13:08 -0300

    top-library

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,15 +14,48 @@
 
 // console.log(player2.welcomeMessage());
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = () => {
-        return this.title + " by " + this.author + ", " + this.pages + ", " + this.read
-    }
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+//     this.info = () => {
+//         return this.title + " by " + this.author + ", " + this.pages + ", " + this.read
+//     }
+// }
+
+// const book = new Book("House of Leaves", "Mark z. Danielewski", "500", "read");
+// console.log(book.info());
+
+// console.log(player.valueOf());
+
+function Person(name) {
+    this.name = name;
 }
 
-const book = new Book("House of Leaves", "Mark z. Danielewski", "500", "read");
-console.log(book.info());
+Person.prototype.sayName = function() {
+    console.log(`Hello, I'm ${this.name}!`)
+}
+
+function Player(name, marker) {
+    this.name = name; 
+    this.marker = marker;
+}
+
+Player.prototype.getMarker = function() {
+    console.log(`My marker is '${this.marker}'`)
+}
+
+console.log(Object.getPrototypeOf(Player.prototype));
+
+console.log(Object.setPrototypeOf(Player.prototype, Person.prototype));
+console.log(Object.getPrototypeOf(Player.prototype));
+
+const player1 = new Player('steve', 'x');
+const player2 = new Player('Alex', 'o');
+
+player1.sayName(); // Hello, I'm steve!
+player2.sayName(); // Hello, I'm also steve!
+
+player1.getMarker(); // My marker is 'X'
+player2.getMarker(); // My marker is 'O'
