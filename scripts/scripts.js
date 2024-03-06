4c96933de15c2b6f604d379c3592f4adba8ba892
AndoniRoque <roque.andoni@gmail.com>
2024-03-06 07:48:44 -0300

    jet set

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,61 +1,31 @@
-// function Player(name, marker) {
-//     this.name = name;
-//     this.marker = marker;
-//     this.welcomeMessage = () => {
-//         console.log("hello ", this.name, " your maker is ", this.marker);
-//     }
-// }
+const myLibrary = [];
 
-// const player = new Player("steve", "x");
-// console.log(player.name);
-// console.log(player.welcomeMessage())
+addBookToLibrary(createBook(params));
+displayBooks();
 
-// const player2 = new Player("alex", "0");
-
-// console.log(player2.welcomeMessage());
-
-// function Book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-//     this.info = () => {
-//         return this.title + " by " + this.author + ", " + this.pages + ", " + this.read
-//     }
-// }
-
-// const book = new Book("House of Leaves", "Mark z. Danielewski", "500", "read");
-// console.log(book.info());
-
-// console.log(player.valueOf());
-
-function Person(name) {
-    this.name = name;
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () => {
+        return this.title + " by " + this.author + ", " + this.pages + ", " + this.read
+    }
 }
 
-Person.prototype.sayName = function() {
-    console.log(`Hello, I'm ${this.name}!`)
+function createBook(params) {
+    // recieve book information through form
+    const book = new Book(params)
+    return book;
 }
 
-function Player(name, marker) {
-    this.name = name; 
-    this.marker = marker;
+function addBookToLibrary(book) {
+    return myLibrary.push(book);
 }
 
-Player.prototype.getMarker = function() {
-    console.log(`My marker is '${this.marker}'`)
+function displayBooks() {
+    myLibrary.forEach(book => {
+        // display book title in #container
+    });
 }
 
-console.log(Object.getPrototypeOf(Player.prototype));
-
-console.log(Object.setPrototypeOf(Player.prototype, Person.prototype));
-console.log(Object.getPrototypeOf(Player.prototype));
-
-const player1 = new Player('steve', 'x');
-const player2 = new Player('Alex', 'o');
-
-player1.sayName(); // Hello, I'm steve!
-player2.sayName(); // Hello, I'm also steve!
-
-player1.getMarker(); // My marker is 'X'
-player2.getMarker(); // My marker is 'O'
