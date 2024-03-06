ad4a06a50bf1aeaae7bf314731953d4131dcd2ea
AndoniRoque <roque.andoni@gmail.com>
2024-03-06 13:40:42 -0300

    book cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
-const dune = new Book("Dune", "Frank Herber", 500, "Yes");
-const rayuela = new Book("Rayuela", "Julio Cortazar", 600,"Yes");
+const dune = new Book(0, "Dune", "Frank Herbert", 500, "Yes");
+const rayuela = new Book(1, "Rayuela", "Julio Cortazar", 600,"Yes");
 
 const dialog = document.querySelector("dialog");
 const newBookButton = document.querySelector("#newBook");
@@ -7,9 +7,11 @@ const closeButton = document.querySelector("#closeBook");
 const uploadBook = document.querySelector("#uploadBook");
 const form = document.querySelector("form");
 const outputBox = document.querySelector("output");
+const showLibrary = document.querySelector("#showLibrary");
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(id, title, author, pages, read) {
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -29,31 +31,71 @@ uploadBook.addEventListener("click", (event) => {
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
     const read = document.querySelector("#read").checked;
-    const book = new Book(title,author, pages, read ? "yes" : "no");
+    const id = createId();
+    const book = new Book(id, title, author, pages, read ? "yes" : "no");
     addBookToLibrary(book);
-    console.log(myLibrary);
+    
     dialog.close(book.info());
 })
 
-dialog.addEventListener("close", (e) => {
-    outputBox.value = dialog.returnValue = `${dialog.returnValue} `;
-})
+console.log(myLibrary);
 
 closeButton.addEventListener("click", () =>{
     dialog.close();
 })
+
+showLibrary.addEventListener('click', () => {
+    displayLibrary(myLibrary);
+})
+
+function createId() {
+    return myLibrary.length == 0 ? 0 : myLibrary.length + 1; 
+}
 
 function addBookToLibrary(book) {
     return myLibrary.push(book);
 }
 
 function displayLibrary() {
+    const bookContainer = document.createElement('div');
+    bookContainer.className = "bookContainer";
+    outputBox.textContent = "";
+
     myLibrary.forEach(book => {
-        outputBox.textContent += book.info() + " | ";
+        const bookCard = document.createElement('div');
+        const titleCard = document.createElement('h3');
+        const authorCard = document.createElement('h6');
+        const readCard = document.createElement('div');
+        const bookNro = document.createElement('div');
+        const deleteBook = document.createElement('button');
+
+        bookCard.className = "bookCard";
+        bookNro.className = "bookIndex";
+
+        deleteBook.textContent = "X";
+        bookNro.textContent = book.id;
+        titleCard.textContent = book.title;
+        authorCard.textContent = book.author;
+        readCard.textContent = book.read;
+
+
+        deleteBook.addEventListener('click', () => {
+            console.log(book.id);
+            myLibrary.splice(book.id, 1);
+        })
+        
+        
+        bookCard.appendChild(bookNro);
+        bookCard.appendChild(titleCard);
+        bookCard.appendChild(authorCard);
+        bookCard.appendChild(readCard);
+        bookCard.appendChild(deleteBook);
+        
+        bookContainer.appendChild(bookCard);
+        
+        outputBox.appendChild(bookContainer);
     });
 }
-
-
 
 addBookToLibrary(dune);
 addBookToLibrary(rayuela);
